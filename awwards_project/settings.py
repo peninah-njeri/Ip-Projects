6c90e0381808b5a5c1878389876deb33c610556a
peninah-njeri <peninahgathuru@gmail.com>
2019-11-23 16:53:53 +0300

    define my app configurations inside the project so as to connect to the templates

--- awwards_project/settings.py
+++ awwards_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'awwards.apps.AwwardsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
