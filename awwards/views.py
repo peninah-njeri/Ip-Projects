e974231a89ee06d507af4ecc167a923be2cdd49a
peninah-njeri <peninahgathuru@gmail.com>
2019-11-24 12:47:33 +0300

    add static file for images and custom styling and also install bootstrap for styling too.

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
