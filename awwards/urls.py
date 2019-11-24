7dab5e3465849eb6016d8c1a7173d6328dcf417a
peninah-njeri <peninahgathuru@gmail.com>
2019-11-24 22:41:11 +0300

    Define a url for search results, which allows a user to search for a project

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
      url('^$',views.index,name = 'index'), 
      url(r'^project/(\d+)', views.project, name = 'project'), 
+     url(r'^profile/(\d+)', views.profile, name = 'profile'),
+     url(r'^search/', views.search_results, name='search_results'),
+]  
 
-]    
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
