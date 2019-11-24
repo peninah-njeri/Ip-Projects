b0d087a3ad5cac19b7ed86d225d207c95f47e01d
peninah-njeri <peninahgathuru@gmail.com>
2019-11-24 13:55:59 +0300

    update the projects urlspatterns to add the urlconf for the tinymce package

--- awwards_project/settings.py
+++ awwards_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'awwards.apps.AwwardsConfig',
     'bootstrap3',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
