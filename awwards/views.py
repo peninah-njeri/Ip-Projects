c92459a3a8fb486046f7a54ce0e0e95920a1e1a1
peninah-njeri <peninahgathuru@gmail.com>
2019-11-26 00:47:04 +0300

    create api views to handle the requests

--- awwards/views.py
+++ awwards/views.py
@@ -93,3 +93,37 @@ def rate_project(request,id):
     else:
         form = RateForm()
     return render(request, 'rate.html',{"form": form, 'proj':project})
+
+
+
+
+class ProfileList(APIView):
+    # permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProjectList(APIView):
+    # permission_classes = (IsAdminOrReadOnly,)
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
